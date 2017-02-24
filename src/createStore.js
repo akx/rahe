8d84a3ff3fa90a5ba13a00d4da9a5d7e08cf1c5e
Aarni Koskela <akx@iki.fi>
2017-02-24 16:54:10 +0200

    Packagify reducers

--- src/createStore.js
+++ src/createStore.js
@@ -1,27 +1,8 @@
 /* eslint-env browser */
-import {compose, createStore, combineReducers} from 'redux';
+import {compose, createStore} from 'redux';
 import persistState, {mergePersistedState} from 'redux-localstorage';
 import adapter from 'redux-localstorage/lib/adapters/localStorage';
-import R from 'ramda';
-import {notification} from 'antd';
-
-const transactionsReducer = (state = [], action) => {
-  if (action.type === 'ADD_TRANSACTIONS') {
-    const currentTxnIds = new Set(R.pluck('id', state));
-    const txnsToAdd = action.payload.filter((txn) => !currentTxnIds.has(txn.id));
-    const newTxns = R.sortBy(R.prop('id'), state.concat(txnsToAdd));
-
-    notification.success({
-      message: 'Transaction data saved',
-      description: `${txnsToAdd.length} new transactions; ${newTxns.length} total transactions.`,
-    });
-    return newTxns;
-  }
-  return state;
-};
-const rootReducer = combineReducers({
-  transactions: transactionsReducer,
-});
+import rootReducer from './reducers';
 
 
 export default function () {
---
+++ src/reducers/transactions.js
@@ -0,0 +1,18 @@
+/* eslint-env browser */
+import R from 'ramda';
+import {notification} from 'antd';
+
+export default function transactionsReducer(state = [], action) {
+  if (action.type === 'ADD_TRANSACTIONS') {
+    const currentTxnIds = new Set(R.pluck('id', state));
+    const txnsToAdd = action.payload.filter((txn) => !currentTxnIds.has(txn.id));
+    const newTxns = R.sortBy(R.prop('id'), state.concat(txnsToAdd));
+
+    notification.success({
+      message: 'Transaction data saved',
+      description: `${txnsToAdd.length} new transactions; ${newTxns.length} total transactions.`,
+    });
+    return newTxns;
+  }
+  return state;
+};
