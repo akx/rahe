e9c6d2c7eeac38f9757b65f759d6ca1b4585b529
Aarni Koskela <akx@iki.fi>
2017-05-19 14:02:25 +0300

    Use enUS locale provider

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+import antdCss from 'antd/dist/antd.min.css';
 
 import createStore from './createStore';
 import App from './App';
+import localCss from './style.css';
 
-import antdCss from 'antd/dist/antd.min.css';  // eslint-disable-line
-import localCss from './style.css';  // eslint-disable-line
+const x = [antdCss, localCss];  // eslint-disable-line
 
 const store = createStore();
 
 
 const root = (
   <Provider store={store}>
-    <App />
+    <LocaleProvider locale={enUS}>
+      <App />
+    </LocaleProvider>
   </Provider>
 );
 
